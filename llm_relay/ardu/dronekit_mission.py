5b466fc8007dc38824ecebf2379399d2d9ae6970
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-07-16 21:22:07 -0500

    Added dronekit mission script for ardu relay and updated README with instructions for ardupilot and drone kit mission script.

---
+++ llm_relay/ardu/dronekit_mission.py
@@ -0,0 +1,36 @@
+from dronekit import connect, VehicleMode
+import time
+
+# Connect to the Vehicle
+vehicle = connect('udp:127.0.0.1:14551',wait_ready=True)
+
+print("arming the vehicle")
+while not vehicle.is_armable:
+    print("waiting for vehicle to become armable")
+    time.sleep(1)
+
+vehicle.mode = VehicleMode("GUIDED")
+vehicle.armed = True
+
+print("takeoff")
+
+vehicle.simple_takeoff(10)
+
+while True:
+    print("Altitude: ", vehicle.location.global_relative_frame.alt)
+    if vehicle.location.global_relative_frame.alt >= 9.5:
+        print("target altitude reached")
+        break
+    time.sleep(1)
+
+print("landing")
+vehicle.mode = VehicleMode("LAND")
+
+while vehicle.location.global_relative_frame.alt >= 0.5:
+    print("Altitude: ", vehicle.location.global_relative_frame.alt)
+    time.sleep(1)
+
+print("disarming")
+vehicle.armed = False
+
+
