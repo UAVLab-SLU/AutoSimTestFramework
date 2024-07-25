9fb666b137c169438d7f7ea3a1335eb224067e6f
Bohan Zhang <bohan.zhang.1@slu.edu>
2024-07-25 16:46:21 -0500

    Add: ardupilot flight script and log

--- SuT/ardu/standalone/rq1-2.py
+++ SuT/ardu/standalone/rq1-2.py
@@ -1,29 +1,45 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative
-import time
-
+from dronekit import connect, VehicleMode,  LocationGlobalRelative, LocationGlobal
+import time, math
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two `LocationGlobal` or `LocationGlobalRelative` objects.
+    This method is an approximation, and will not be accurate over large distances and close to the
+    earth's poles. It comes from the ArduPilot test code:
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 # Connect to the Vehicle
 vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True)
 print("Connected to the vehicle!")
 
+# get current geo-coordinates
+print("Current position: ", vehicle.location.global_frame)
+
+lat = vehicle.location.global_frame.lat
+lon = vehicle.location.global_frame.lon
+alt = vehicle.location.global_frame.alt
+
+
 waypoints = [
-    LocationGlobalRelative(27.994402, -82.582034, 5),
-    LocationGlobalRelative(27.994402, -82.583034, 5),
-    LocationGlobalRelative(27.995402, -82.583034, 5),
-    LocationGlobalRelative(27.995402, -82.582034, 5),
-    LocationGlobalRelative(27.994402, -82.582034, 5)
+    LocationGlobalRelative(lat + 0.001, lon, 5),
+    LocationGlobalRelative(lat + 0.001, lon + 0.001, 5),
+    LocationGlobalRelative(lat, lon + 0.001, 5),
+    LocationGlobalRelative(lat, lon, 5)
 ]
 
 wind_vector = [3.5, 3.5, 0]
 vehicle.airspeed = 3
 
-# Set home location (uncomment if needed, adjust coordinates)
-vehicle.home_location = vehicle.location.global_frame
-vehicle.home_location.lat = 27.994402
-vehicle.home_location.lon = -82.582034
 
 print("Arming the vehicle")
 vehicle.mode = VehicleMode("GUIDED")
 vehicle.armed = True
+
+
+
+
 
 while not vehicle.armed:
     print("Waiting for arming...")
@@ -43,16 +59,15 @@ while True:
 for point in waypoints:
     print("Going to:", point)
     vehicle.simple_goto(point)
-
-    # Wait for the drone to reach the target location
-    while vehicle.mode.name=="GUIDED":  # Check if still in GUIDED mode
-        remaining_distance = vehicle.location.global_frame.distance_to(point)
-        print("Distance to target:", remaining_distance)
-        if remaining_distance <= 1:
+    # Wait for the drone to get close to the target point
+    while True:
+        dist = get_distance_metres(vehicle.location.global_frame, point)
+        print("Distance to target:", dist)
+        if dist <= 2:
             print("Reached target")
             break
-
         time.sleep(1)
+
 
 print("Landing")
 vehicle.mode = VehicleMode("LAND")
